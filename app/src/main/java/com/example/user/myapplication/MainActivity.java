4b2e00dc0993dd90281aa25c5853932d561df35e
ksh001479 <ksh001479@daum.net>
2018-09-16 21:39:14 +0900

    두번째_커밋

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.user.myapplication;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
@@ -19,12 +23,23 @@ import com.google.zxing.common.BitMatrix;
 import java.util.EnumMap;
 import java.util.Map;
 
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+
 public class MainActivity extends AppCompatActivity {
+
+    //블루투스 SSP통신
+    private BluetoothSPP bt;
     // 배경 색상
     private static final int WHITE = 0xFFFFFFFF;
     // 바코드 색상
     private static final int BLACK = 0xFF000000;
     private static ImageView barcode;
+
+    int randomNum;
+
+    //Button barButton;
 
     private Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
         String contentsToEncode = contents;
@@ -74,14 +89,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void makeBar() {
         barcode = findViewById(R.id.imageView);
-        double randomNum = Math.random()*100000;
+        int randomNum = (int) (Math.random() * 100000);
         randomNum *= 10000;
+        this.randomNum = randomNum;
         String barcode_data = String.valueOf(randomNum); //바코드 모양
+        Log.d(String.valueOf(randomNum),"바코드값");
 
         // barcode image
 
         try {
-            Bitmap bitmap = encodeAsBitmap(barcode_data , BarcodeFormat.CODE_128 , 600 , 300);
+            Bitmap bitmap = encodeAsBitmap(barcode_data, BarcodeFormat.CODE_128, 600, 300);
             barcode.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
@@ -92,13 +109,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button barButton = (Button)findViewById(R.id.button);
-        Button phpButton = (Button)findViewById(R.id.button2);
+        Button barButton = (Button) findViewById(R.id.button);
+        Button phpButton = (Button) findViewById(R.id.button2);
+
+//        barButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                makeBar();
+//                bt.send(String.valueOf(randomNum), true);
+//            }
+//        });
 
         barButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                makeBar();
+            public void onClick(View view) {
+                setup();
             }
         });
 
@@ -111,8 +136,105 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        /////////////블루투스
+        bt = new BluetoothSPP(this);
+
+        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
+            public void onDataReceived(byte[] data, String message) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(getApplicationContext()
+                        , "Connected to " + name + "\n" + address
+                        , Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceDisconnected() { //연결해제
+                Toast.makeText(getApplicationContext()
+                        , "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceConnectionFailed() { //연결실패
+                Toast.makeText(getApplicationContext()
+                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnConnect = findViewById(R.id.btnConnect); //연결시도
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+    }
+////////////////////////////블루투스 함수
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService(); //블루투스 중지
     }
 
+    public void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) { //
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if (!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+                setup();
+
+            }
+        }
+    }
+
+//    public void setup() {
+//        //Button btnSend = findViewById(R.id.btnSend); //데이터 전송
+//        Button button = findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                bt.send(String.valueOf(randomNum), true);
+//            }
+//        });
+//    }
+
+    public void setup(){
+                makeBar();
+                bt.send(String.valueOf(randomNum), true);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+                setup();
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 
 
 }
