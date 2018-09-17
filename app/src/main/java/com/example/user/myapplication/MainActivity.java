b1dc4781154032fa629f8e544dc7a525a83f51f8
USER <gkfhak123@gmail.com>
2018-09-17 19:06:07 +0900

    android_php

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,14 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -36,9 +45,11 @@ public class MainActivity extends AppCompatActivity {
     // 바코드 색상
     private static final int BLACK = 0xFF000000;
     private static ImageView barcode;
-
     int randomNum;
 
+    String myCode;
+    int myCount;
+    String myTime;
     //Button barButton;
 
     private Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
@@ -130,10 +141,14 @@ public class MainActivity extends AppCompatActivity {
         phpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://limong.iptime.org/~m30509/stac/test.php"));
-                startActivity(myIntent);
+                myCode= String.valueOf(randomNum); //문자열
+                myCount = Integer.parseInt(((EditText)findViewById(R.id.count)).getText().toString()); //숫자
+                myTime = ((EditText)(findViewById(R.id.time))).getText().toString();      //날짜
+
+                HttpPostData();   // 서버와 자료 주고받기
             }
         });
+
 
 
         /////////////블루투스
@@ -182,6 +197,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //php 전송
+    public void HttpPostData() {
+        try {
+            //--------------------------
+            //   URL 설정하고 접속하기
+            //--------------------------
+            URL url = new URL("http://limong.iptime.org/~m30509/stac/insert_bar.php");       // URL 설정
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
+            //--------------------------
+            //   전송 모드 설정 - 기본적인 설정이다
+            //--------------------------
+            http.setDefaultUseCaches(false);
+            http.setDoInput(true);                         // 서버에서 읽기 모드 지정
+            http.setDoOutput(true);                       // 서버로 쓰기 모드 지정
+            http.setRequestMethod("POST");         // 전송 방식은 POST
+
+            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+            //--------------------------
+            //   서버로 값 전송
+            //--------------------------
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("code").append("=").append(myCode).append("&");                 // php 변수에 값 대입
+            buffer.append("count").append("=").append(myCount).append("&");   // php 변수 앞에 '$' 붙이지 않는다
+            buffer.append("time").append("=").append(myTime).append("&");           // 변수 구분은 '&' 사용
+
+            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
+            PrintWriter writer = new PrintWriter(outStream);
+            writer.write(buffer.toString());
+            writer.flush();
+            //--------------------------
+            //   서버에서 전송받기
+           /* //--------------------------
+            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR");
+            BufferedReader reader = new BufferedReader(tmp);
+            StringBuilder builder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
+                builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
+            }
+            myResult = builder.toString();                       // 전송결과를 전역 변수에 저장
+            ((TextView)(findViewById(R.id.text_result))).setText(myResult);
+            Toast.makeText(MainActivity.this, "전송 후 결과 받음", 0).show();
+            */
+        } catch (MalformedURLException e) {
+            //
+        } catch (IOException e) {
+            //
+        } // try
+    }
+
 ////////////////////////////블루투스 함수
     public void onDestroy() {
         super.onDestroy();
