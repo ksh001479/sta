6f544dbf5528dbe76d0ac6ae7fd14d8ef3fdd2ab
ksh001479 <ksh001479@daum.net>
2018-09-25 16:53:10 +0900

    바코드값 DB연결

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,14 +20,6 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -45,11 +36,10 @@ public class MainActivity extends AppCompatActivity {
     // 바코드 색상
     private static final int BLACK = 0xFF000000;
     private static ImageView barcode;
-    int randomNum;
 
-    String myCode;
-    int myCount;
-    String myTime;
+    int randomNum;
+    public static String getPhp;
+
     //Button barButton;
 
     private Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
@@ -100,11 +90,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void makeBar() {
         barcode = findViewById(R.id.imageView);
-        int randomNum = (int) (Math.random() * 100000);
+        randomNum = (int) (Math.random() * 100000);
         randomNum *= 10000;
-        this.randomNum = randomNum;
         String barcode_data = String.valueOf(randomNum); //바코드 모양
-        Log.d(String.valueOf(randomNum),"바코드값");
+
+        getPhp = "http://limong.iptime.org/~m30509/stac/GetPhp.php?code="+barcode_data;
+
+        Log.d(barcode_data,"바코드값");
 
         // barcode image
 
@@ -123,184 +115,130 @@ public class MainActivity extends AppCompatActivity {
         Button barButton = (Button) findViewById(R.id.button);
         Button phpButton = (Button) findViewById(R.id.button2);
 
-//        barButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                makeBar();
-//                bt.send(String.valueOf(randomNum), true);
-//            }
-//        });
 
         barButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                setup();
+            public void onClick(View v) {
+                makeBar();
+                //bt.send(String.valueOf(randomNum), true);
             }
         });
+
+        //블루투스 버튼
+//        barButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                setup();
+//            }
+//        });
 
         phpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myCode= String.valueOf(randomNum); //문자열
-                myCount = Integer.parseInt(((EditText)findViewById(R.id.count)).getText().toString()); //숫자
-                myTime = ((EditText)(findViewById(R.id.time))).getText().toString();      //날짜
-
-                HttpPostData();   // 서버와 자료 주고받기
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getPhp));
+                startActivity(myIntent);
             }
         });
 
 
-
-        /////////////블루투스
-        bt = new BluetoothSPP(this);
-
-        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
-            Toast.makeText(getApplicationContext()
-                    , "Bluetooth is not available"
-                    , Toast.LENGTH_SHORT).show();
-            finish();
-        }
-
-        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
-            public void onDataReceived(byte[] data, String message) {
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
-            public void onDeviceConnected(String name, String address) {
-                Toast.makeText(getApplicationContext()
-                        , "Connected to " + name + "\n" + address
-                        , Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceDisconnected() { //연결해제
-                Toast.makeText(getApplicationContext()
-                        , "Connection lost", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceConnectionFailed() { //연결실패
-                Toast.makeText(getApplicationContext()
-                        , "Unable to connect", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button btnConnect = findViewById(R.id.btnConnect); //연결시도
-        btnConnect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
-                    bt.disconnect();
-                } else {
-                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
-                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
-                }
-            }
-        });
-    }
-
-    //php 전송
-    public void HttpPostData() {
-        try {
-            //--------------------------
-            //   URL 설정하고 접속하기
-            //--------------------------
-            URL url = new URL("http://limong.iptime.org/~m30509/stac/insert_bar.php");       // URL 설정
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
-            //--------------------------
-            //   전송 모드 설정 - 기본적인 설정이다
-            //--------------------------
-            http.setDefaultUseCaches(false);
-            http.setDoInput(true);                         // 서버에서 읽기 모드 지정
-            http.setDoOutput(true);                       // 서버로 쓰기 모드 지정
-            http.setRequestMethod("POST");         // 전송 방식은 POST
-
-            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
-            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
-            //--------------------------
-            //   서버로 값 전송
-            //--------------------------
-            StringBuffer buffer = new StringBuffer();
-            buffer.append("code").append("=").append(myCode).append("&");                 // php 변수에 값 대입
-            buffer.append("count").append("=").append(myCount).append("&");   // php 변수 앞에 '$' 붙이지 않는다
-            buffer.append("time").append("=").append(myTime).append("&");           // 변수 구분은 '&' 사용
-
-            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
-            PrintWriter writer = new PrintWriter(outStream);
-            writer.write(buffer.toString());
-            writer.flush();
-            //--------------------------
-            //   서버에서 전송받기
-           /* //--------------------------
-            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR");
-            BufferedReader reader = new BufferedReader(tmp);
-            StringBuilder builder = new StringBuilder();
-            String str;
-            while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
-                builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
-            }
-            myResult = builder.toString();                       // 전송결과를 전역 변수에 저장
-            ((TextView)(findViewById(R.id.text_result))).setText(myResult);
-            Toast.makeText(MainActivity.this, "전송 후 결과 받음", 0).show();
-            */
-        } catch (MalformedURLException e) {
-            //
-        } catch (IOException e) {
-            //
-        } // try
-    }
-
-////////////////////////////블루투스 함수
-    public void onDestroy() {
-        super.onDestroy();
-        bt.stopService(); //블루투스 중지
-    }
-
-    public void onStart() {
-        super.onStart();
-        if (!bt.isBluetoothEnabled()) { //
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
-        } else {
-            if (!bt.isServiceAvailable()) {
-                bt.setupService();
-                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
-                setup();
-
-            }
-        }
-    }
-
-//    public void setup() {
-//        //Button btnSend = findViewById(R.id.btnSend); //데이터 전송
-//        Button button = findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
+//        /////////////블루투스
+//        bt = new BluetoothSPP(this);
+//
+//        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+//            Toast.makeText(getApplicationContext()
+//                    , "Bluetooth is not available"
+//                    , Toast.LENGTH_SHORT).show();
+//            finish();
+//        }
+//
+//        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
+//            public void onDataReceived(byte[] data, String message) {
+//                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+//            public void onDeviceConnected(String name, String address) {
+//                Toast.makeText(getApplicationContext()
+//                        , "Connected to " + name + "\n" + address
+//                        , Toast.LENGTH_SHORT).show();
+//            }
+//
+//            public void onDeviceDisconnected() { //연결해제
+//                Toast.makeText(getApplicationContext()
+//                        , "Connection lost", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            public void onDeviceConnectionFailed() { //연결실패
+//                Toast.makeText(getApplicationContext()
+//                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        Button btnConnect = findViewById(R.id.btnConnect); //연결시도
+//        btnConnect.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View v) {
-//                bt.send(String.valueOf(randomNum), true);
+//                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+//                    bt.disconnect();
+//                } else {
+//                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+//                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+//                }
 //            }
 //        });
 //    }
-
-    public void setup(){
-                makeBar();
-                bt.send(String.valueOf(randomNum), true);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK)
-                bt.connect(data);
-        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
-            if (resultCode == Activity.RESULT_OK) {
-                bt.setupService();
-                bt.startService(BluetoothState.DEVICE_OTHER);
-                setup();
-            } else {
-                Toast.makeText(getApplicationContext()
-                        , "Bluetooth was not enabled."
-                        , Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
+//////////////////////////////블루투스 함수
+//    public void onDestroy() {
+//        super.onDestroy();
+//        bt.stopService(); //블루투스 중지
+//    }
+//
+//    public void onStart() {
+//        super.onStart();
+//        if (!bt.isBluetoothEnabled()) { //
+//            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+//        } else {
+//            if (!bt.isServiceAvailable()) {
+//                bt.setupService();
+//                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+//                setup();
+//
+//            }
+//        }
+//    }
+//
+////    public void setup() {
+////        //Button btnSend = findViewById(R.id.btnSend); //데이터 전송
+////        Button button = findViewById(R.id.button);
+////        button.setOnClickListener(new View.OnClickListener() {
+////            public void onClick(View v) {
+////                bt.send(String.valueOf(randomNum), true);
+////            }
+////        });
+////    }
+//
+//    public void setup(){
+//                makeBar();
+//                bt.send(String.valueOf(randomNum), true);
+//    }
+//
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+//            if (resultCode == Activity.RESULT_OK)
+//                bt.connect(data);
+//        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                bt.setupService();
+//                bt.startService(BluetoothState.DEVICE_OTHER);
+//                setup();
+//            } else {
+//                Toast.makeText(getApplicationContext()
+//                        , "Bluetooth was not enabled."
+//                        , Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        }
     }
 
 
